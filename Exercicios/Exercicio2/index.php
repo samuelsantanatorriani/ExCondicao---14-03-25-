23becef8bae45991c7a32132f14dfdbd96f1c018
EmillyChristinny <emillychristinnyal@gmail.com>
2025-03-14 08:40:49 -0300

    Realização dos exercicios 1 e 2

---
+++ Exercicios/Exercicio2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Exercicio 2</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <fieldset>
+        <form method="post" action="calcular.php">
+
+            Digite a primeira nota:
+            <input type="text" name="v1" size="25" /><br><br>  
+            
+            Digite a segunda nota:
+            <input type="text" name="v2" size="25" /><br><br> 
+
+            Digite a terceira nota:
+            <input type="text" name="v3" size="25" /><br><br>  
+            
+            Digite a quarta nota:
+            <input type="text" name="v4" size="25" /><br><br> 
+
+
+            <input type="submit" name="area" value="calcular"/>
+    </form>
+    </fieldset> 
+</body>
+</html>
